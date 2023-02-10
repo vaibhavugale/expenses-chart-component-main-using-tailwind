10868494bc6215ba6385ef634570742668aeac22
vaibhavugale <ugalevaibhav00@gmail.com>
2023-02-10 15:50:09 +0530

    First Depolyment

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'faintRed': '#E37B5D',
+        'DarkBrown':'#52473D',
+        'cream':'#F8EADC'
+      },
+    },
+  },
+  plugins: [],
+}
